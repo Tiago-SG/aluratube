891940fa11b2b123fe261a48d35dc9aab22e5079
Tiago-SG <tiagosg2599@gmail.com>
2022-11-08 16:25:42 -0300

    Adição de banner e favoritos

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,10 @@ function HomePage() {
         <>
             <CSSReset />
             <div style={estiloDaHome}>
-                <Menu valorDoFiltro = {valorDoFiltro} setValorDoFiltro = {setValorDoFiltro}/>
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
                 <Header />
-                <TimeLine searchValue = {valorDoFiltro} playlists={config.playlists} />
+                <TimeLine searchValue={valorDoFiltro} playlists={config.playlists} />
+                <Favs favorits={config.favorits} />
             </div>
         </>
     )
@@ -31,35 +32,70 @@ const StyledHeader = styled.div`
         height: 80px;
         border-radius: 50%;
     }
+
+    .bn{
+        width: 100%; 
+        height: 200px;
+    }
+
     .user-info{
-        margin-top: 50px;
-        dusplay: flex;
-        align-item: center;
+        display: flex;
+        align-items: center;
         width: 100%;
         padding: 16px 32px;
         gap: 16px;
     }
 `;
 
+const StyledBanner = styled.div`
+    background-color: blue;
+    background-image: url(${config.banner});
+    height: 230px;
+`;
+
+const StyledFav = styled.span`
+    img{
+        margin-top: 10px;
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+    }
+    a{
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-items: center;
+        padding: 8px;
+    }
+    span{
+        font-weight: 600;
+        font-size: 14px;
+        text-align: center;
+        padding-top: 4px;
+        color: ${({ theme }) => theme.textColorBase || "#222222"};
+    }
+`;
+
 function Header() {
     return (
         <StyledHeader>
+            <StyledBanner />
             <section className="user-info">
                 <img src={config.linkedin} />
-                <div>
+                <a>
                     <h2>
                         {config.name}
                     </h2>
                     <p>
                         {config.job}
                     </p>
-                </div>
+                </a>
             </section>
         </StyledHeader>
     )
 }
 
-function TimeLine({searchValue, ...props}) {
+function TimeLine({ searchValue, ...props }) {
     const playlistNames = Object.keys(props.playlists);
     return (
         <StyledTimeline>
@@ -88,5 +124,25 @@ function TimeLine({searchValue, ...props}) {
                 )
             })}
         </StyledTimeline>
+    )
+}
+
+function Favs(props) {
+    const favorits = Object.keys(props.favorits);
+    return (
+        <StyledFav>
+            <h2>
+                Favoritos
+            </h2>
+            {favorits.map((favorits) => {
+                const perfil = props.favorits[favorits];
+                return (
+                    <a key={perfil.url} href={perfil.url}>
+                        <img src={perfil.photo} />
+                        <span> {perfil.name} </span>
+                    </a>
+                )
+            })}
+        </StyledFav>
     )
 }

31ebfa02e6566ee69f138ccc181c66ad1eb9a40b
Tiago-SG <tiagosg2599@gmail.com>
2022-11-08 10:42:17 -0300

    Inclusão da busca

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
+import react from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
-import Menu from "../src/components/Menu";
+import Menu from "../src/components/Menu/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
     const estiloDaHome = { //backgroundColor: "red" 
     };
 
+    const [valorDoFiltro, setValorDoFiltro] = react.useState("");
+
     return (
         <>
             <CSSReset />
             <div style={estiloDaHome}>
-                <Menu />
+                <Menu valorDoFiltro = {valorDoFiltro} setValorDoFiltro = {setValorDoFiltro}/>
                 <Header />
-                <TimeLine playlists={config.playlists} />
+                <TimeLine searchValue = {valorDoFiltro} playlists={config.playlists} />
             </div>
         </>
     )
@@ -56,19 +59,23 @@ function Header() {
     )
 }
 
-function TimeLine(props) {
+function TimeLine({searchValue, ...props}) {
     const playlistNames = Object.keys(props.playlists);
     return (
         <StyledTimeline>
             {playlistNames.map((playlistName) => {
                 const videos = props.playlists[playlistName];
                 return (
-                    <section>
+                    <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
-                            {videos.map((video) => {
+                            {videos.filter((video) => {
+                                const titleNormalized = video.title.toLowerCase();
+                                const searchValueNormalized = searchValue.toLowerCase();
+                                return titleNormalized.includes(searchValueNormalized)
+                            }).map((video) => {
                                 return (
-                                    <a href={video.url}>
+                                    <a key={video.url} href={video.url}>
                                         <img src={video.thumb} />
                                         <span>
                                             {video.title}
